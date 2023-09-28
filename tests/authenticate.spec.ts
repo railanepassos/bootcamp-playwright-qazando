0ba013725b7090ba03391f828d0a996f99032eab
Railane Passos <railanepassos@gmail.com>
2023-09-28 18:38:27 -0300

    add the screanshots in test case

--- tests/authenticate.spec.ts
+++ tests/authenticate.spec.ts
@@ -3,12 +3,15 @@ import { test, expect } from '@playwright/test';
 test('successfully login @login @regression', async ({ page }) => {
   test.setTimeout(20000);
   await page.goto('https://automationpratice.com.br/');
+  await page.screenshot({ path: `screenshots/successfullyLogin01.png` })
   await page.getByRole('link', { name: ' Login' }).click();
   await page.locator('#user').click();
   await page.locator('#user').fill('teste@teste.com.br');
   await page.locator('#password').click();
   await page.locator('#password').fill('123456');
+  await page.screenshot({ path: `screenshots/successfullyLogin02.png` })
   await page.getByRole('button', { name: 'login' }).click();
   await page.getByRole('button', { name: 'OK' }).click();
+  await page.screenshot({ path: `screenshots/successfullyLogin03.png` })
 
 })
