a49acb95c276d1d6edb174ff65efd78f8171c429
Railane Passos <railanepassos@gmail.com>
2023-10-09 09:13:06 -0300

    update test scenario

--- tests/authenticate.spec.ts
+++ tests/authenticate.spec.ts
@@ -2,8 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('successfully login @login @regression', async ({ page }) => {
   test.setTimeout(20000);
+
+  // Given the user is on the home page
   await page.goto('https://automationpratice.com.br/');
   await page.screenshot({ path: `screenshots/successfullyLogin01.png` })
+
+  // When the user autenticate with valid credentials
   await page.getByRole('link', { name: ' Login' }).click();
   await page.locator('#user').click();
   await page.locator('#user').fill('teste@teste.com.br');
@@ -11,6 +15,8 @@ test('successfully login @login @regression', async ({ page }) => {
   await page.locator('#password').fill('123456');
   await page.screenshot({ path: `screenshots/successfullyLogin02.png` })
   await page.getByRole('button', { name: 'login' }).click();
+
+  // Then the user should be logged in
   await page.getByRole('button', { name: 'OK' }).click();
   await page.screenshot({ path: `screenshots/successfullyLogin03.png` })
 
